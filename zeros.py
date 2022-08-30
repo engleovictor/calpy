c4d650d1b2dd2f4aa980392dd429ca9e32ded74c
unknown <leotpw9@gmail.com>
2022-08-30 18:14:55 -0300

    Refactor

--- zeros.py
+++ zeros.py
@@ -1,74 +1,116 @@
-#   Feito por Léo
-#   19/08/2022
-#   lib feita para calculo numerico
+## Criado Por Léo
+## 19/08/22
+## Mod 28/08/22
+## Lib Feita pra Calculo Numérico
 
-m = abs
+def bisection(f,x0=1e-5,x1=1e5,iterations=1000):
+    x = (x1+x0)/2
+    if f(x) == 0.0:
+        return [x,1,0.]
+    else:
+        for i in range(2,iterations+1):
+            var = f(x)
+            
+            if var*f(x0) < 0:
+                x1 = x
+            else:
+                x0 = x
 
-def secant(f, x1=-1e3, x0=1e3,min_value = 1e-8, max_num_iter=10000):
-    i = 0 
-    c1 = x1
-    c0 = x0
-    for i in range(max_num_iter):
-        var = c1
-        c1 = c1 - f(c1)*(c1-c0)/(f(c1)-f(c0))
-        c0 = var
+            x = (x1+x0)/2
 
-        if abs(f(c1)) < min_value:
-            break
+            err = (x1-x0)/2
+            
+            if f(x) == 0.0:
+                return [x,i,err]
 
-    return [c1,i]
+        return [x,iterations,err]
 
-def bissec(f, a=-1e5, b=1e5, min_value = 1e-8, max_num_iter=10000):
-    i = 0 
+def fakeposition(f,x0=1e-5,x1=1e5,iterations=1000):    
+    x = (x1*abs(f(x0))+x0*abs(f(x1)))/(abs(f(x0))+abs(f(x1)))
+    if f(x) == 0.0:
+        return [x,1,0.]
+    else:
+        for i in range(2,iterations+1):
+            var = f(x)
+            
+            if var*f(x0) < 0:
+                x1 = x
+            else:
+                x0 = x
+
+            x = (x1*abs(f(x0))+x0*abs(f(x1)))/(abs(f(x0))+abs(f(x1)))
+
+            err = max(abs(x1*f(x0)/(f(x0)-f(x1))), abs(x0*f(x1)/(f(x0)-f(x1))))
+            
+            if f(x) == 0.0:
+                return [x,i,0.]
+
+        return [x,iterations,err]
     
-    for i in range(max_num_iter):
-        c = (a+b)/2
-        if f(a)*f(c) < 0:
-            b = c
-        else:
-            a = c
+def fixedpoint(f,x0=None,iterations=1000):
+    if not x0:
+        exit('Precisa do valor de x0!!')
+    
+    x = f(x0)
 
-        if abs(f(c)) < min_value:
-            break
+    if x == f(x):
+        return [x,1,0.]
+    else:
+        for i in range(2,iterations+1):
+            j = 0
+            var = x
+            x = f(x)
+            err = abs(x - var)
+            if x == f(x):
+                return [x,i,0.]
+            else:
+                if abs(f(x)) - abs(f(var)) > 1:
+                   j+=1
+                   if j > 5:
+                    exit('\nEscolha outra funcao de iteracao.\nEssa não converge!!')
+                                
+        return [x,iterations,err]
+                
 
-    return [c,i]
+def newtonraphson(f,df=None,x0=None,iterations=1000):
+    if not x0:
+        exit('Precisamos de um valor de x0')
 
-def falpos(f, a=-1e5, b=1e5, min_value = 1e-8, max_num_iter=10000):
-    i = 0
+    x = x0
 
-    for i in range(max_num_iter):
-        c = (m(f(b))*a - m(f(a))*b)/(f(b)-f(a))
-        if f(a)*f(c) < 0:
-            b = c
-        else:
-            a = c
+    if df == None:
+        df = lambda x: (f(x+1e-10)-f(x))/1e-10
+    elif type(df) == "<class 'function'>":
+        exit('Preciso de uma função derivada válida!!')
 
-        if abs(f(c)) < min_value:
-            break
+    for i in range(1,iterations+1):
+        if f(x) == 0.0:
+            return [x,i,0.]
+        
+        var = x
 
-    return [c,i]
+        x = x - f(x)/df(x)
 
-def mpfixo(f, cfv=0, min_value = 1e-8, max_num_iter=10000):
-    i = 0
-    c = cfv
+        err = abs(x-var)
 
-    for i in range(max_num_iter):
-        c = f(c)
+    return [x,i,err]
 
-        if abs(f(c)) < min_value:
-            break
+def secant(f,x0=1e-5,x1=1e5,iterations=1000):
+    var = x1
+    x1 = x1 - f(x1)*(x1-x0)/(f(x1)-f(x0))
+    x0 = var
+    if f(x1) == 0.0:
+        return [x1,1,0.]
+    else:
+        for i in range(2,iterations+1):
+            var = x1
+            x1 = x1 - f(x1)*(x1-x0)/(f(x1)-f(x0))
+            x0 = var
 
-    return [c,i]
+            err = x1 - x0
 
-def newrap(f, cfv=0,min_value = 1e-8, max_num_iter=10000):
-    df = lambda x: (f(x+1e-8)-f(x))/1e-8
-    i = 0
-    c = cfv
-    for i in range(max_num_iter):
-        c -= f(c)/df(c)
+            if f(x1) == 0.0:
+                return [x1,i,0.]
 
-        if abs(f(c)) < min_value:
-            break
-
-    return [c,i]
+    return [x1,i,err]
 
